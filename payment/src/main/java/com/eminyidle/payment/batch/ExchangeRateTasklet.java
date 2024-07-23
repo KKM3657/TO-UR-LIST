cdcbcbc6a92fcbb561ea446c38ea8063c4a56066
김강민 <mmm3657@naver.com>
2024-07-24 01:44:29 +0900

    fix: 주석 제거 및 커밋 확인

--- payment/src/main/java/com/eminyidle/payment/batch/ExchangeRateTasklet.java
+++ payment/src/main/java/com/eminyidle/payment/batch/ExchangeRateTasklet.java
@@ -37,12 +37,6 @@ public class ExchangeRateTasklet implements Tasklet {
 
     @Override
     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-        // 만약 이미 데이터가 있다면 넘어감
-//        List<ExchangeRate> DuplicateExchangeDateList = batchService.loadExchangeRateList();
-//        if (!DuplicateExchangeDateList.isEmpty()) {
-//            log.debug("금일 환율 데이터는 이미 반영되어 있습니다.");
-//            return RepeatStatus.FINISHED;
-//        }
         return retryTemplate.execute(new RetryCallback<RepeatStatus, Exception>() {
             @Override
             public RepeatStatus doWithRetry(RetryContext context) throws Exception {
@@ -85,27 +79,5 @@ public class ExchangeRateTasklet implements Tasklet {
                 return RepeatStatus.FINISHED;
             }
         });
-
-//        RestTemplate restTemplate = new RestTemplate();
-//
-//        HttpHeaders headers = new HttpHeaders();
-//        MediaType mediaType = new MediaType("application", "x-www-form-urlencoded", StandardCharsets.UTF_8);
-//        headers.setContentType(mediaType);
-//
-//        // 요청 매개변수 설정
-//        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
-//
-//        String url = "https://v6.exchangerate-api.com/v6/" + authKey + "/latest/KRW";
-//        // 환율 요청
-//        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
-//
-//        // 응답 처리
-//        String responseBody = response.getBody();
-//        log.info("Response Body: {}", responseBody);
-//
-//        // 환율 정보 저장
-//        batchService.saveExchangeRates(responseBody);
-//
-//        return RepeatStatus.FINISHED;
     }
 }
